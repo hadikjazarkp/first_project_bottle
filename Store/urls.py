889c19a3920fd7534016be8a77c5c942cce302fe
hadik <hadik987789@gmail.com>
2023-10-13 09:14:16 +0530

    store

---
+++ Store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+     
+    path("", views.home, name="home"),
+    path("shop", views.shop, name="shop"),
+    path("shop/<str:slug>", views.shopview, name="shopview"),
+]
---
+++ Store/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
+
+
+def home(request):
+    return render(request,"store/index.html")
+
+
+def shop(request):
+    category = Category.objects.filter(status=0)
+    context = {'category':category}
+    return render(request, "store/shop.html", context)
+
+
+def shopview(request, slug):
+    if(Category.objects.filter(slug=slug, status=0)):
+        products = Product.objects.filter(category__slug=slug)
+        category_name = Category.objects.filter(slug=slug).first()
+        context = {'products': products, 'category_name':category_name}
+        return render(request, "store/products/index.html", context)
+    else:
+        messages.warning(request, "No surch category found")
+        return redirect('shop')
+        
